3c5117404a3a71af9a8ea3434c5833273a663809
Jahija303 <jahijao@gmail.com>
2025-08-04 15:23:42 +0200

    Refactoring BlazePose

--- gymcoach/app.js
+++ gymcoach/app.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
-const sharp = require('sharp');
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'css')));
 app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
