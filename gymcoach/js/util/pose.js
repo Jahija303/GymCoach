0f1fcf0678d77acb798b844fc5e10504a899fc35
Edna <ednapasanbegovic6@gmail.com>
2025-08-21 14:31:49 +0200

    3D Stickman inverted fix, Using heavy model

--- gymcoach/js/util/pose.js
+++ gymcoach/js/util/pose.js
@@ -43,7 +43,9 @@ export class Pose {
 
         let poseLandmarker = await PoseLandmarker.createFromOptions(vision, {
             baseOptions: {
-                modelAssetPath: "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task",
+                modelAssetPath: "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_heavy/float16/1/pose_landmarker_heavy.task",
+                // modelAssetPath: "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_full.task",
+                // modelAssetPath: "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task",
                 delegate: "GPU"
             },
             runningMode: "VIDEO",
